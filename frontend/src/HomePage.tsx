866719e9f048e5a9fccfd2c0138ad86e1fb99e8d
adinteressant <rochak.adh98@gmail.com>
2025-04-20 23:55:51 +0545

    authorization

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,12 +1,30 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from './authContext'
 import { Link } from 'react-router-dom'
 import { UserContext } from './userContext'
 const HomePage = () => {
-const {isLoggedIn} = useContext(AuthContext)
-const {user} = useContext(UserContext)
+  const [post,setPost] = useState<string>('')
+  useEffect(()=>{
+    const fetchPosts = async ():Promise<void> => {
+      try{
+        const response = await fetch(`/api/posts`)
+        const data = await response.json()
+        setPost(data.post) 
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+    fetchPosts()
+  },[])
+  const {isLoggedIn} = useContext(AuthContext)
+  const {user} = useContext(UserContext)
   if(isLoggedIn){
   return <div>welcome {user.username}
+
+  <div>
+  {post}
+  </div>
   <div>
   <Link to={'/login'}>
     logout
